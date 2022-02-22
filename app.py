0cd9fdcb9f25c95afaecc75b28bad8dae5dae6b8
João Paulo Araujo Braz <jparaujobraz84@gmail.com>
2022-02-21 23:03:22 -0300

    datetime and select types update

--- app.py
+++ app.py
@@ -2,8 +2,10 @@ import pandas as pd
 import streamlit as st
 from fbprophet import Prophet
 import pymannkendall as mk
+from fbprophet.plot import add_changepoints_to_plot
 
 
+@st.cache(allow_output_mutation=True)
 def converter_arquivo_em_dataframe(uploaded_file):
     if uploaded_file.type.endswith('csv'):
         data = pd.read_csv(uploaded_file, sep=";")
@@ -14,7 +16,7 @@ def converter_arquivo_em_dataframe(uploaded_file):
     return data
 
 
-CHOICES_TYPE_PERIOD = {'Y': 'Anual', 'm': 'Mensal', 'd': 'Diário'}
+CHOICES_TYPE_PERIOD = {'Y': 'Ano', 'm': 'Mês', 'd': 'Dia', 'H': 'Hora'}
 
 
 def format_func(option):
@@ -33,7 +35,8 @@ if uploaded_file is not None:
     data = converter_arquivo_em_dataframe(uploaded_file)
 
     if data is not None:
-
+        # variaveis para campos de atributos das predições
+        qnt_max = int(len(data.index)*0.20)
 
         # fig, ax = plt.subplots()
         # data.hist(bins=8, column=coluna_dados, grid=False, figsize=(4, 4), color="#86bf91", zorder=2, rwidth=0.9, ax=ax, )
@@ -43,13 +46,14 @@ if uploaded_file is not None:
         st.sidebar.subheader("Defina os atributos para predição")
 
         # mapeando dados do usuário para cada atributo
-        periodo_dados = st.sidebar.selectbox("Coluna da Data", options=list(data.select_dtypes(exclude='int64')))
-        coluna_dados = st.sidebar.selectbox("Coluna de Dados", options=list(data.select_dtypes(include='int64')))
+        periodo_dados = st.sidebar.selectbox("Coluna da Data", options=list(data.select_dtypes(exclude=['int64', 'float64'])))
+        coluna_dados = st.sidebar.selectbox("Coluna de Dados", options=list(data.select_dtypes(include=['int64', 'float64'])))
         tipo_periodo_predicao = st.sidebar.selectbox("Tipo de Periodo para Predição",
                                                      options=list(CHOICES_TYPE_PERIOD.keys()), format_func=format_func)
-        periodo_predicao = st.sidebar.slider(label='Período para predição', min_value=1, max_value=20, value=5)
+        periodo_predicao = st.sidebar.slider(label='Período para predição', min_value=0, max_value=qnt_max, value=1)
+
         st.info(
-            f"Selecionada a coluna '{coluna_dados}' com predição '{format_func(tipo_periodo_predicao)}' para '{periodo_predicao}' períodos")
+            f"Selecionada a coluna '{coluna_dados}' com predição por '{format_func(tipo_periodo_predicao)}' para '{periodo_predicao}' períodos")
 
         if st.checkbox('Mostrar primeiros dados da planilha', ):
             st.subheader('Head dos dados ')
@@ -63,16 +67,23 @@ if uploaded_file is not None:
 
             # verifica se o botão foi acionado
             if btn_predict:
+                data[periodo_dados] = pd.to_datetime(data[periodo_dados])
+                data[periodo_dados] = data[periodo_dados].dt.strftime('%Y-%m-%d %H:%M:%S')
+                # print(data[periodo_dados].to_list)
+
                 # Prophet
-                df = pd.DataFrame.from_records(data)
+                #df2 = pd.DataFrame.from_records(data)
+                df = data.copy()
                 df.rename(columns={periodo_dados: 'ds', coluna_dados: 'y'}, inplace=True)
                 m = Prophet()
                 m.fit(df)
                 future = m.make_future_dataframe(periods=periodo_predicao, freq=tipo_periodo_predicao, include_history=True)
                 # future.tail()
-                futuro_predito = forecast = m.predict(future)
+                forecast = m.predict(future)
                 # forecast[['ds', 'yhat', 'yhat_lower', 'yhat_upper']]
-                fig1 = m.plot(forecast, xlabel='Ano', ylabel='Casos')
+                fig1 = m.plot(forecast, xlabel=format_func(tipo_periodo_predicao), ylabel='Frequência')
+                a = add_changepoints_to_plot(fig1.gca(), m, forecast)
+
                 fig2 = m.plot_components(forecast)
 
                 # st.subheader('Dados')
@@ -88,5 +99,9 @@ if uploaded_file is not None:
                 result = mk.original_test(dados)
 
                 st.subheader("Resultado do pyMannKendall")
-                st.success(result)
-                st.balloons()
+                if result.trend == 'no trend':
+                    st.error(result)
+                else:
+                    st.success(result)
+                    st.write("Parabens! Seu dados tem uma dendência de: ", result.trend)
+                    st.balloons()
